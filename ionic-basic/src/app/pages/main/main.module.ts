e8369069f39a42d0ca412ea9ea2498f362e7cab5
Barbara <rossana.pesil@gmail.com>
2024-05-05 03:13:51 -0600

    P5: Menu funcional

--- ionic-basic/src/app/pages/main/main.module.ts
+++ ionic-basic/src/app/pages/main/main.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { MainPageRoutingModule } from './main-routing.module';
 
 import { MainPage } from './main.page';
+import { ShareModule } from 'src/app/share/share.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MainPageRoutingModule
+    MainPageRoutingModule,
+    ShareModule
   ],
   declarations: [MainPage]
 })
